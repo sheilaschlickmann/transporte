321bbe3faa2a96dba87e5304136d855bf6f9a814
CSS2919 <silva.christian@gmail.com>
2022-11-18 20:18:19 -0300

    Commit Entregador

---
+++ src/main/java/com/unibave/transporte/Entity/Entregador.java
@@ -0,0 +1,33 @@
+package com.unibave.transporte.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "entregador")
+@JsonIgnoreProperties(ignoreUnknown = true, value = {"hibernateLazyInitializer", "handler"})
+@Data
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Entregador {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(name = "nome")
+    @NotEmpty(message = "O nome da bandeira é obrigatório")
+    @Size(max = 100, message = "O tamanho do nome da bandeira não pode passar de 100 caracteres")
+    private String nome;
+
+}
