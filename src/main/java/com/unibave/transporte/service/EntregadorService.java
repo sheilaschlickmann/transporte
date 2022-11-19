8cc241e139f697f009f0e8ab8a997d49be28f3a6
CSS2919 <silva.christian@gmail.com>
2022-11-19 10:01:40 -0300

    segundo commmit

--- src/main/java/com/unibave/transporte/service/EntregadorService.java
+++ src/main/java/com/unibave/transporte/service/EntregadorService.java
@@ -1,10 +1,14 @@
 package com.unibave.transporte.service;
 
 import com.unibave.transporte.entity.Entregador;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Positive;
 import java.util.List;
 
+@Service
+@Validated
 public interface EntregadorService {
     Entregador buscarPor(
             @Positive(message = "Não pode ser menos que 0!")
